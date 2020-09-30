44cb6246e5d6e2170b8c24812b1d56685268c0a7
Wander Hillen <wjw.hillen@gmail.com>
2020-09-30 11:58:04 +0200

    Convert specs into test descriptions

--- spec/lib/fileslide_streamer_spec.rb
+++ spec/lib/fileslide_streamer_spec.rb
@@ -5,14 +5,26 @@ RSpec.describe FileslideStreamer do
     FileslideStreamer.new
   end
 
-  it 'redirects to the main site on index' do
-    get '/'
-    expect(last_response.status).to eq 302
+  context 'utility endpoints' do
+    it 'redirects to the main site on index' do
+      get '/'
+      expect(last_response.status).to eq 302
+    end
+
+    it 'returns 200/All good on the healthcheck' do
+      get 'healthcheck'
+      expect(last_response.status).to eq 200
+      expect(last_response.body).to eq "All good\n"
+    end
   end
 
-  it 'returns 200/All good on the healthcheck' do
-    get 'healthcheck'
-    expect(last_response.status).to eq 200
-    expect(last_response.body).to eq "All good\n"
+  context '/download' do
+    it 'fails with 400 if the request is not valid JSON'
+    it 'fails with 400 if the request JSON does not contain a file_name key'
+    it 'fails with 400 if the request JSON does not contain a uri_list key'
+    it 'fails with 403 and the returned html if the upstream API does not authorize the download'
+    it 'fails with 502 and a list of failed URIs is any of the URIs are not 200/206'
+    it 'streams the uris as a zip'
+    it 'deduplicates filenames if multiple files have the same name'
   end
 end
