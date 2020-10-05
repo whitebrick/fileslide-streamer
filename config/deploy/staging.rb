7bb462e7fb0e64e63a7dd72cc83ba3bf44fca48a
Wander Hillen <wjw.hillen@gmail.com>
2020-10-05 12:55:12 +0200

    Update hostname of staging server for capistrano

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,3 @@
 set :stage, :staging
 set :default_env, 'RACK_ENV' => 'staging'
-server '3.87.220.24', user: 'ec2-user', roles: %w{web app db}
+server 'stream.fileslide-staging.io', user: 'ec2-user', roles: %w{web app db}
