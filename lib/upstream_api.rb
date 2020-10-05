d91dcd1a712ffd9bce84607378a886fb73cf21e3
Wander Hillen <wjw.hillen@gmail.com>
2020-10-05 13:38:02 +0200

    Prevent double conversion to JSON and use idiomatic http.rb

--- lib/upstream_api.rb
+++ lib/upstream_api.rb
@@ -11,8 +11,8 @@ class UpstreamAPI
   end
 
   def verify_uri_list(uri_list:, file_name: )
-    auth_response = @http.post("#{UPSTREAM_API_LOCATION}/authorize", json: {uri_list: uri_list, file_name: file_name}.to_json)
-    unless auth_response.status == 200
+    auth_response = @http.post("#{UPSTREAM_API_LOCATION}/authorize", json: {uri_list: uri_list, file_name: file_name})
+    unless auth_response.status.ok?
       raise UpstreamNotFoundError
     end
     auth_body_json = JSON.parse(auth_response.to_s, symbolize_names: true)
