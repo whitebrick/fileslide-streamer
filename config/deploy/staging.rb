754b4ac24870398f0c424f0763b6957357e80eb2
Wander Hillen <wjw.hillen@gmail.com>
2020-10-30 13:30:18 +0100

    Set Puma to 2 workers, 5 threads each in staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,5 @@
 set :stage, :staging
 set :default_env, 'RACK_ENV' => 'staging'
+set :puma_workers, 2
+set :puma_threads, [0, 5]
 server 'stream.fileslide-staging.io', user: 'ec2-user', roles: %w{web app db}
