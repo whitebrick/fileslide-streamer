094937386d1e390cd5fd9e7ba384c08796c160f4
Wander Hillen <wjw.hillen@gmail.com>
2020-11-29 14:35:58 +0100

    Give staging pumas configs more threads and workers and bind explicitly to 0.0.0.0:9292

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,7 @@
 set :stage, :staging
 set :default_env, 'RACK_ENV' => 'staging'
-set :puma_workers, 2
-set :puma_threads, [0, 5]
+set :puma_workers, 3
+set :puma_threads, [0, 8]
+set :puma_bind, "tcp://0.0.0.0:9292"
 server '3.227.183.196', user: 'ec2-user', roles: %w{web app db}
 server '34.199.63.18', user: 'ec2-user', roles: %w{web app db}
